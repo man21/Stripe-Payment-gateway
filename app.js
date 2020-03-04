0127f5f7e39734facea4502f765afadba32a358c
Mandeep <yadavmandeep96@gmail.com>
2020-03-04 17:53:08 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,227 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const pug = require('pug');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+require("dotenv").config()
+
+const keyPublishable = process.env.STRIPE_PUB_KEY;
+const keySecret = process.env.STRIPE_SECRET_KEY;
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+const stripe = require('stripe')(keySecret);
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.use(logger('dev'));
+app.use(
+  express.json({
+    // We need the raw body to verify webhook signatures.
+    // Let's compute it only when hitting the Stripe webhook endpoint.
+    verify: function (req, res, buf) {
+      if (req.originalUrl.startsWith("/webhook")) {
+        req.rawBody = buf.toString();
+      }
+    }
+  })
+);
+app.use(express.urlencoded({
+  extended: false
+}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+app.get("/stripe-key", (req, res) => {
+  res.send({
+    publishableKey: keyPublishable
+  });
+});
+
+app.post("/charge", function (req, res) {
+
+  let amount = 5 * 100; // 500 cents means $5
+
+  // create a customer
+  stripe.customers.create({
+      email: req.body.stripeEmail, // customer email, which user need to enter while making payment
+      source: req.body.stripeToken // token for the given card
+    })
+    .then(customer =>
+      stripe.charges.create({ // charge the customer
+        amount,
+        description: "Sample Charge",
+        currency: "usd",
+        customer: customer.id
+      }))
+    .then(charge =>
+      res.render("charge")
+    )
+    .catch((error) => {
+      console.log(error.type);
+    }); // render the charge view: views/charge.pug
+
+});
+
+app.get('/complete/:cid', async(req, res) => {
+  console.log(req.params.cid, "Req params cid ")
+  const customer = await stripe.customers.retrieve(req.params.cid)
+
+  res.render("complete", {customer: JSON.stringify(customer, null, 2)})
+})
+
+app.post('/create-customer', async (req, res) => {
+
+  stripe.customers.create({
+      payment_method: req.body.payment_method,
+      name: req.body.name,
+      email: req.body.email,
+      phone: req.body.phone,
+      invoice_settings: {
+        default_payment_method: req.body.payment_method
+      },
+    }).then(async (result) => {
+      const subscription = await stripe.subscriptions.create({
+        customer: result.id,
+        items: [{
+          plan: req.body.plan
+        }],
+        billing_cycle_anchor: 1583416492,
+      })
+    
+    
+      const invoice = await stripe.invoices.retrieve(subscription.latest_invoice)
+      const paymentIntent = await stripe.paymentIntents.retrieve(invoice.payment_intent)
+    
+      const customer = await stripe.customers.retrieve(result.id)
+
+      res.json({
+        success: true,
+        customer,
+        subscription,
+        paymentIntent,
+        invoice
+      });
+    })
+    .catch((err) => {
+      return res.status(402).json(err);
+    })
+
+
+})
+
+
+app.get("/publishable-key", async (req, res) => {
+
+  res.json({
+    publicKey: keyPublishable
+  })
+})
+
+app.post("/retrieve-payment", async (req, res) => {
+  stripe.paymentMethods.retrieve(
+    req.body.pm_id,
+    function (err, paymentMethod) {
+      // asynchronously called
+      res.json(paymentMethod);
+    }
+  );
+})
+
+app.get("/checkout", async (req, res) => {
+  res.render("checkout");
+});
+
+app.post("/pay", async (req, res) => {
+  const {
+    paymentMethodId,
+    paymentIntentId,
+    items,
+    currency,
+    useStripeSdk
+  } = req.body;
+
+  const orderAmount = 25;
+
+  try {
+    let intent;
+    if (paymentMethodId) {
+      // Create new PaymentIntent with a PaymentMethod ID from the client.
+      intent = await stripe.paymentIntents.create({
+        amount: orderAmount,
+        currency: currency,
+        payment_method: paymentMethodId,
+        confirmation_method: "manual",
+        confirm: true,
+        // If a mobile client passes `useStripeSdk`, set `use_stripe_sdk=true`
+        // to take advantage of new authentication features in mobile SDKs
+        //use_stripe_sdk: useStripeSdk,
+      });
+      // After create, if the PaymentIntent's status is succeeded, fulfill the order.
+    } else if (paymentIntentId) {
+      // Confirm the PaymentIntent to finalize payment after handling a required action
+      // on the client.
+      intent = await stripe.paymentIntents.confirm(paymentIntentId);
+      // After confirm, if the PaymentIntent's status is succeeded, fulfill the order.
+    }
+    res.send(generateResponse(intent));
+  } catch (e) {
+    // Handle "hard declines" e.g. insufficient funds, expired card, etc
+    // See https://stripe.com/docs/declines/codes for more
+    res.send({
+      error: e.message
+    });
+  }
+});
+
+const generateResponse = intent => {
+  // Generate a response based on the intent's status
+  switch (intent.status) {
+    case "requires_action":
+    case "requires_source_action":
+      // Card requires authentication
+      return {
+        requiresAction: true,
+          clientSecret: intent.client_secret
+      };
+    case "requires_payment_method":
+    case "requires_source":
+      // Card was not properly authenticated, suggest a new payment method
+      return {
+        error: "Your card was denied, please provide a new payment method"
+      };
+    case "succeeded":
+      // Payment is complete, authentication not required
+      // To cancel the payment after capture you will need to issue a Refund (https://stripe.com/docs/api/refunds)
+      console.log("💰 Payment received!");
+      return {
+        clientSecret: intent.client_secret
+      };
+  }
+};
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ public/js/index.js
@@ -0,0 +1,417 @@
+'use strict';
+
+var stripe;
+var elements;
+const amount = document.getElementById("amount").value;
+const plan = document.getElementById("plan").value;
+const orderData = {
+  amount: amount,
+  currency: "usd"
+};
+
+const setupElements = function (data) {
+  stripe = Stripe(data.publicKey);
+  elements = stripe.elements({
+    fonts: [{
+      cssSrc: 'https://fonts.googleapis.com/css?family=Quicksand',
+    }, ],
+    // Stripe's examples are localized to specific languages, but if
+    // you wish to have Elements automatically detect your user's locale,
+    // use `locale: 'auto'` instead.
+    locale: window.__exampleLocale,
+  });
+
+  var elementStyles = {
+    base: {
+      color: '#fff',
+      fontWeight: 600,
+      fontFamily: 'Quicksand, Open Sans, Segoe UI, sans-serif',
+      fontSize: '16px',
+      fontSmoothing: 'antialiased',
+
+      ':focus': {
+        color: '#424770',
+      },
+
+      '::placeholder': {
+        color: '#9BACC8',
+      },
+
+      ':focus::placeholder': {
+        color: '#CFD7DF',
+      },
+    },
+    invalid: {
+      color: '#fff',
+      ':focus': {
+        color: '#FA755A',
+      },
+      '::placeholder': {
+        color: '#FFCCA5',
+      },
+    },
+  };
+
+  var elementClasses = {
+    focus: 'focus',
+    empty: 'empty',
+    invalid: 'invalid',
+  };
+
+  var cardNumber = elements.create('cardNumber', {
+    style: elementStyles,
+    classes: elementClasses,
+  });
+  cardNumber.mount('#example3-card-number');
+
+  var cardExpiry = elements.create('cardExpiry', {
+    style: elementStyles,
+    classes: elementClasses,
+  });
+  cardExpiry.mount('#example3-card-expiry');
+
+  var cardCvc = elements.create('cardCvc', {
+    style: elementStyles,
+    classes: elementClasses,
+  });
+  cardCvc.mount('#example3-card-cvc');
+
+  return {
+    stripe: stripe,
+    card: cardNumber
+  };
+};
+
+const exampleName = "example3"
+var formClass = '.' + exampleName;
+var example = document.querySelector(formClass);
+
+var form = example.querySelector('form');
+var resetButton = example.querySelector('a.reset');
+var error = form.querySelector('.error');
+var errorMessage = error.querySelector('.message');
+
+function enableInputs() {
+  Array.prototype.forEach.call(
+    form.querySelectorAll(
+      "input[type='text'], input[type='email'], input[type='tel']"
+    ),
+    function (input) {
+      input.removeAttribute('disabled');
+    }
+  );
+}
+
+function disableInputs() {
+  Array.prototype.forEach.call(
+    form.querySelectorAll(
+      "input[type='text'], input[type='email'], input[type='tel']"
+    ),
+    function (input) {
+      input.setAttribute('disabled', 'true');
+    }
+  );
+}
+
+function triggerBrowserValidation() {
+  // The only way to trigger HTML5 form validation UI is to fake a user submit
+  // event.
+  var submit = document.createElement('input');
+  submit.type = 'submit';
+  submit.style.display = 'none';
+  form.appendChild(submit);
+  submit.click();
+  submit.remove();
+}
+
+// Listen for errors from each Element, and show error messages in the UI.
+/*
+var savedErrors = {};
+elements.forEach(function (element, idx) {
+  element.on('change', function (event) {
+    if (event.error) {
+      error.classList.add('visible');
+      savedErrors[idx] = event.error.message;
+      errorMessage.innerText = event.error.message;
+    } else {
+      savedErrors[idx] = null;
+
+      // Loop over the saved errors and find the first one, if any.
+      var nextError = Object.keys(savedErrors)
+        .sort()
+        .reduce(function (maybeFoundError, key) {
+          return maybeFoundError || savedErrors[key];
+        }, null);
+
+      if (nextError) {
+        // Now that they've fixed the current error, show another one.
+        errorMessage.innerText = nextError;
+      } else {
+        // The user fixed the last error; no more errors.
+        error.classList.remove('visible');
+      }
+    }
+  });
+});
+*/
+
+var showError = function (errorMsgText) {
+  error.classList.add('visible');
+  errorMessage.innerText = errorMsgText;
+  setTimeout(function () {
+    errorMessage.textContent = "";
+    error.classList.remove('visible');
+  }, 10000);
+};
+
+// Listen on the form's 'submit' handler...
+const submitForm = (data) => {
+  form.addEventListener('submit', function (e) {
+    e.preventDefault();
+
+    // Trigger HTML5 validation UI on the form if any of the inputs fail
+    // validation.
+    var plainInputsValid = true;
+    Array.prototype.forEach.call(form.querySelectorAll('input'), function (
+      input
+    ) {
+      if (input.checkValidity && !input.checkValidity()) {
+        plainInputsValid = false;
+        return;
+      }
+    });
+    if (!plainInputsValid) {
+      triggerBrowserValidation();
+      return;
+    }
+
+    // Show a loading screen...
+    example.classList.add('submitting');
+
+    // Disable all inputs.
+    disableInputs();
+
+    // Gather additional customer data we may have collected in our form.
+    var name = form.querySelector('#' + exampleName + '-name');
+    var email = form.querySelector('#' + exampleName + '-email');
+    var phone = form.querySelector('#' + exampleName + '-phone');
+    var address1 = form.querySelector('#' + exampleName + '-address');
+    var city = form.querySelector('#' + exampleName + '-city');
+    var state = form.querySelector('#' + exampleName + '-state');
+    var zip = form.querySelector('#' + exampleName + '-zip');
+    var additionalData = {
+      name: name ? name.value : undefined,
+      email: email ? email.value : undefined,
+      phone: phone ? phone.value : undefined,
+      line1: address1 ? address1.value : undefined,
+      city: city ? city.value : undefined,
+      state: state ? state.value : undefined,
+      postal_code: zip ? zip.value : undefined,
+    };
+    data.additionalData = additionalData
+    pay(data);
+
+    // Use Stripe.js to create a token. We only need to pass in one Element
+    // from the Element group in order to create a token. We can also pass
+    // in the additional customer data we collected in our form.
+
+
+    /*
+      stripe.createToken(elements[0], additionalData).then(function (result) {
+        // Stop loading!
+        example.classList.remove('submitting');
+    
+        if (result.token) {
+          // If we received a token, show the token ID.
+          console.log(result)
+          example.querySelector('.token').innerText = result.token.id;
+          example.classList.add('submitted');
+        } else {
+          // Otherwise, un-disable inputs.
+          enableInputs();
+        }
+      });
+      */
+  });
+}
+
+const redirect = (cid) => {
+  setTimeout(function () {
+    window.location = "/complete/"+cid
+  }, 2000);
+}
+
+function stripePaymentMethodHandler(result) {
+  if (result.error) {
+    // Show error in payment form
+  } else {
+    // Otherwise send paymentMethod.id to your server
+    fetch('/create-customer', {
+        method: 'post',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          name: result.paymentMethod.billing_details.name,
+          email: result.paymentMethod.billing_details.email,
+          phone: result.paymentMethod.billing_details.phone,
+          payment_method: result.paymentMethod.id,
+          plan: plan
+        }),
+      }).then(function (result) {
+        return result.json();
+      }).then(function (response) {
+        // The customer has been created
+        if (response.type === "StripeCardError") {
+          example.classList.remove('submitting');
+          showError("Your card was declined!");
+        } else {
+          const {
+            client_secret,
+            status
+          } = response.paymentIntent;
+
+          if (status === 'requires_action' || status === "requires_payment_method") {
+            stripe.confirmCardPayment(client_secret).then(function (result) {
+              example.classList.remove('submitting');
+              if (result.error) {
+                // Display error message in your UI.
+                // The card was declined (i.e. insufficient funds, card has expired, etc)
+                example.classList.remove('submitted');
+                showError(result.error.message);
+              } else {
+                // Show a success message to your customer
+                example.classList.add('submitted');
+                redirect(response.customer.id)
+                //var responseJson = JSON.stringify(response.customer, null, 2);
+                //document.querySelector("pre").textContent = responseJson;
+              }
+            });
+          } else {
+            // No additional information was needed
+            // Show a success message to your customer
+            example.classList.add('submitted');
+            redirect(response.customer.id)
+            //var responseJson = JSON.stringify(response.customer, null, 2);
+            //document.querySelector("pre").textContent = responseJson;
+          }
+        }
+
+      })
+      .catch((error) => {
+        example.classList.remove('submitting');
+        showError(error);
+      })
+  }
+}
+
+var pay = function (data) {
+  //changeLoadingState(true);
+
+  // Initiate the payment.
+  // If authentication is required, handleCardPayment will automatically display a modal
+
+  var card = data.card
+  var additionalData = data.additionalData;
+
+  stripe.createPaymentMethod({
+    type: 'card',
+    card: card,
+    billing_details: {
+      name: additionalData.name,
+      email: additionalData.email,
+      phone: additionalData.phone,
+      address: {
+        city: additionalData.city,
+        country: additionalData.country,
+        state: additionalData.state,
+        line1: additionalData.line1,
+        postal_code: additionalData.postal_code,
+      }
+    },
+  }).then(stripePaymentMethodHandler);
+}
+/* ------- Post-payment helpers ------- */
+
+/* Shows a success / error message when the payment is complete */
+var orderComplete = function (clientSecret) {
+  stripe.retrievePaymentIntent(clientSecret).then(function (result) {
+
+    var payment_id = result.paymentIntent.payment_method;
+    //var paymentIntent = result.paymentIntent;
+    //var paymentIntentJson = JSON.stringify(paymentIntent, null, 2);
+    //changeLoadingState(false);
+
+
+    fetch("/retrieve-payment", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+          pm_id: payment_id
+        })
+      })
+      .then(function (result) {
+        return result.json();
+      }).then((response) => {
+        var responseJson = JSON.stringify(response, null, 2);
+        document.querySelector("pre").textContent = responseJson;
+      })
+
+
+
+  });
+};
+
+/*
+resetButton.addEventListener('click', function (e) {
+  e.preventDefault();
+  // Resetting the form (instead of setting the value to `''` for each input)
+  // helps us clear webkit autofill styles.
+  form.reset();
+
+  // Clear each Element.
+  elements.forEach(function (element) {
+    element.clear();
+  });
+
+  // Reset error state as well.
+  error.classList.remove('visible');
+
+  // Resetting the form does not un-disable inputs, so we need to do it separately:
+  enableInputs();
+  example.classList.remove('submitted');
+});
+*/
+fetch("/publishable-key", {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json"
+    }
+  })
+  .then(function (result) {
+    return result.json();
+  })
+  .then(function (data) {
+    return setupElements(data);
+  })
+  .then(function ({
+    stripe,
+    card,
+    clientSecret
+  }) {
+    submitForm({
+      stripe,
+      card,
+      clientSecret
+    });
+    /*
+    document.querySelector("#submit").addEventListener("click", function (evt) {
+      evt.preventDefault();
+      // Initiate payment when the submit button is clicked
+      if (form.reportValidity())
+        pay(stripe, card, clientSecret);
+    });
+    */
+  });
